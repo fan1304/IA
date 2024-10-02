e6504078826a7d180c1611784fcf9889e7e3230e
Fanny Avila <fanny_avila13@outlook.com>
2024-10-02 15:42:49 -0600

    Initial commit

---
+++ Inicio/Prac1.py
@@ -0,0 +1,15 @@
+# Imprimir "Hola, como estas" en Python
+print ("Hola, como estas, soy Fanny")
+
+# Operaciones matematicas basicas 
+a = 22
+b = 21
+c = 23
+suma = a + b + c
+print(f"La suma de {a}, {b} y {c} es: {suma}")
+resta = c - a - b
+print(f"La resta de {c}, {a} y {b} es: {resta}")
+mult = a * b * c
+print(f"La multiplicación de {a}, {b} y {c} es: {mult}")
+div = a / (b / c)
+print(f"La division de {a}, {b} y {c} es: {div}")
